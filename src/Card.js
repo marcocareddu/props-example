90ac4ea325511517b586366040e789ea7ab6c918
marcocareddu <saldumaccu@tuta.io>
2024-01-09 09:11:15 +0100

    Destructuring props

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const Card = (props) => {
+
+  // Destructuring props
+  const {url, title, price, username} = props;
   return (
     <article className="col-3 d-flex flex-column justify-content-center align-items-center">
-      <img src={props.url} alt="immagine di card" className="img-fluid" />
+      <img src={url} alt="immagine di card" className="img-fluid" />
       <div className="text-center w-100">
-        <h6>{props.title}</h6>
+        <h6>{title}</h6>
         <div className="divisore my-3"></div>
-        <h5 className="price fw-bold">{props.price}</h5>
-        <h6>{props.username}</h6>
+        <h5 className="price fw-bold">{price}</h5>
+        <h6>{username}</h6>
         <p className="timer fw-bold">00:05</p>
         <div className="puntata">PUNTA</div>
       </div>
